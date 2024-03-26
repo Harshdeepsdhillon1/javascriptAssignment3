99c6f04819035083e21d4ea2735d37e71cd6a8a3
Harshdeepsdhillon1 <harshdeepsdhillon1@gmail.com>
2024-03-25 22:57:59 -0400

    added images and more ingredients

--- js/script.js
+++ js/script.js
@@ -5,10 +5,28 @@ class Smoothie {
         this.flavor = flavor;
         this.extras = extras;
         this.size = size;
+        this.prices = {
+            snack:3,
+            small: 5,
+            medium: 8,
+            large: 9,
+            xLarge: 10,
+            spinach: 1,
+            proteinPowder: 2,
+            chiaSeeds: 1.5,
+            chocoChips: 2,
+            mapleSyrup: 1.5,
+            tripleblend: 0.5,
+            strawberrySyrup: 1
+        };
     }
 
     getDescription() {
-        return `You ordered a ${this.size} ${this.flavor} smoothie with ${this.extras.join(', ')}.`;
+        let totalPrice = this.prices[this.size];
+        this.extras.forEach(extra => {
+            totalPrice += this.prices[extra];
+        });
+        return `You ordered a ${this.size} ${this.flavor} smoothie with ${this.extras.join(', ')}. Total price: $${totalPrice.toFixed(2)}`;
     }
 }
 
@@ -24,9 +42,14 @@ function createSmoothie() {
     const size = document.querySelector('input[name="size"]:checked').value;
 
     const smoothie = new Smoothie(flavor, extras, size);
-    displaySmoothie(smoothie.getDescription());
+    displaySmoothie(smoothie.getDescription(), flavor);
 }
 
-function displaySmoothie(description) {
-    document.getElementById("smoothieDetails").innerHTML = `<p>${description}</p>`;
+function displaySmoothie(description, flavor) {
+    const smoothieDetails = document.getElementById("smoothieDetails");
+    smoothieDetails.innerHTML = `<p>${description}</p>`;
+    const image = document.createElement('img');
+    image.src = `./images/${flavor}.jpg`;
+    image.alt = `${flavor} Smoothie`;
+    smoothieDetails.appendChild(image);
 }
